11d50c2b510de47989f7e415150ba7f231b98968
Steven Hermans <steven166@live.nl>
2016-06-24 12:16:06 +0200

    Setup docker deployment and development

---
+++ scripts/development.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script for running the MicroDocs Server and UI in a Docker container with a debugger attached and life reload
+
+CONTAINER_NAME=microdocs-dev
+PORT=3000
+
+#docker stop ${CONTAINER_NAME}
+#docker rm ${CONTAINER_NAME}
+
+docker run -d -p ${PORT}:3000 --name ${CONTAINER_NAME} -e "NODE_ENV=development" -v `pwd`:/microdocs -w /microdocs library/node:5.0 scripts/dev_entrypoint.sh
