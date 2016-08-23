fe4014142e97ef100b9e27dc9d06730e21a6b5d1
s.hermans <s.hermans@maxxton.com>
2016-08-23 16:30:22 +0200

    MXTS-242: Add apiblueprint export

--- microdocs-server/src/routes/publish.route.ts
+++ microdocs-server/src/routes/publish.route.ts
@@ -19,88 +19,83 @@ export class PublishRoute extends BaseRoute {
 
   public publishProject(req: express.Request, res: express.Response, next: express.NextFunction) {
     var handler = ResponseHelper.getHandler(req);
-    if (handler == null) {
-      ResponseHelper.getDefaultHandler().handleNotAcceptable(req, res);
-    } else {
-      try {
-        var env = PublishRoute.getEnv(req);
-        if(env == null){
-          handler.handleBadRequest(req, res, "env '" + req.query.env + "' doesn't exists");
-          return;
-        }
-
-        var title = req.params.title;
-        var version = req.query.version;
-        var group = req.query.group;
-        var failOnProblems: boolean = true;
-        if (req.query.failOnProblems == 'false') {
-          failOnProblems = false;
-        }
-
-        //get body
-        var body = null;
-        if(req.get('content-type') == 'application/json'){
-          body = req.body;
-        }else{
-          handler.handleUnsupportedMediaType(req, res);
-          return;
-        }
-
-
-
-        //check request body
-        if (body != null) {
-          var report = req.body as Project;
-
-          //check version is provided
-          if (version == undefined || version == "") {
-            version = SchemaHelper.resolveReference("info.version", report);
-          }
-          if (version == undefined || version == null) {
-            handler.handleBadRequest(req, res, "Missing version parameter");
-            return;
-          }
-
-          //check group is provided
-          if (group == undefined || group == "") {
-            group = SchemaHelper.resolveReference("info.group", report);
-          }
-          if (group == undefined || group == null) {
-            handler.handleBadRequest(req, res, "Missing group parameter");
-            return;
-          }
-
-          //set group and version in the report
-          if (report.info == undefined) {
-            report.info = new ProjectInfo(undefined, undefined, undefined, undefined);
-          }
-          console.info(report.info);
-          report.info.version = version;
-          report.info.versions = [version];
-          report.info.group = group;
-          report.info.title = title;
-
-          // check report
-          var problems: Problem[] = AggregationService.bootstrap().checkProject(env, report);
-
-          if (!(failOnProblems && problems.filter(problem => problem.level == ERROR || problem.level == WARNING).length > 0)) {
-            // save report
-            ReportJsonRepository.bootstrap().storeProject(env, report);
-
-            // run reindex
-            var treeNode = AggregationService.bootstrap().reindex(env);
-          } else {
-            console.warn("Fail publishing due to problems");
-          }
-
-          // return check result
-          handler.handleProblems(req, res, problems, env);
-        } else {
-          handler.handleBadRequest(req, res, "Missing request body");
-        }
-      } catch (e) {
-        handler.handleInternalServerError(req, res, e);
+    try {
+      var env = PublishRoute.getEnv(req);
+      if (env == null) {
+        handler.handleBadRequest(req, res, "env '" + req.query.env + "' doesn't exists");
+        return;
       }
+
+      var title = req.params.title;
+      var version = req.query.version;
+      var group = req.query.group;
+      var failOnProblems: boolean = true;
+      if (req.query.failOnProblems == 'false') {
+        failOnProblems = false;
+      }
+
+      //get body
+      var body = null;
+      if (req.get('content-type') == 'application/json') {
+        body = req.body;
+      } else {
+        handler.handleUnsupportedMediaType(req, res);
+        return;
+      }
+
+
+      //check request body
+      if (body != null) {
+        var report = req.body as Project;
+
+        //check version is provided
+        if (version == undefined || version == "") {
+          version = SchemaHelper.resolveReference("info.version", report);
+        }
+        if (version == undefined || version == null) {
+          handler.handleBadRequest(req, res, "Missing version parameter");
+          return;
+        }
+
+        //check group is provided
+        if (group == undefined || group == "") {
+          group = SchemaHelper.resolveReference("info.group", report);
+        }
+        if (group == undefined || group == null) {
+          handler.handleBadRequest(req, res, "Missing group parameter");
+          return;
+        }
+
+        //set group and version in the report
+        if (report.info == undefined) {
+          report.info = new ProjectInfo(undefined, undefined, undefined, undefined);
+        }
+        console.info(report.info);
+        report.info.version = version;
+        report.info.versions = [version];
+        report.info.group = group;
+        report.info.title = title;
+
+        // check report
+        var problems: Problem[] = AggregationService.bootstrap().checkProject(env, report);
+
+        if (!(failOnProblems && problems.filter(problem => problem.level == ERROR || problem.level == WARNING).length > 0)) {
+          // save report
+          ReportJsonRepository.bootstrap().storeProject(env, report);
+
+          // run reindex
+          var treeNode = AggregationService.bootstrap().reindex(env);
+        } else {
+          console.warn("Fail publishing due to problems");
+        }
+
+        // return check result
+        handler.handleProblems(req, res, problems, env);
+      } else {
+        handler.handleBadRequest(req, res, "Missing request body");
+      }
+    } catch (e) {
+      handler.handleInternalServerError(req, res, e);
     }
   }
 
