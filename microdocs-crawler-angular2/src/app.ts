611dadc21c6ad0e81deb4293529f566bd4b776f4
s.hermans <s.hermans@maxxton.com>
2016-09-23 14:51:25 +0200

    fixed example

--- microdocs-crawler-angular2/src/app.ts
+++ microdocs-crawler-angular2/src/app.ts
@@ -1,5 +1,4 @@
 /// <reference path="../typings/index.d.ts" />
-
 import * as ts from "typescript";
 import * as fs from "fs";
 
@@ -11,8 +10,7 @@ interface DocEntry {
   constructors?: DocEntry[],
   parameters?: DocEntry[],
   returnType?: string
-}
-;
+};
 
 /** Generate documention for all classes in a set of .ts files */
 function generateDocumentation(fileNames: string[], options: ts.CompilerOptions): void {
@@ -31,53 +29,26 @@ function generateDocumentation(fileNames: string[], options: ts.CompilerOptions)
   }
 
   // print out the doc
+  console.info(JSON.stringify(output, undefined, 4));
   fs.writeFileSync("classes.json", JSON.stringify(output, undefined, 4));
 
   return;
 
   /** visit nodes finding exported classes */
   function visit(node: ts.Node) {
-    // Only consider exported nodes
-    if ((<ts.ClassDeclaration>node).name == undefined || (<ts.ClassDeclaration>node).name.kind == undefined) {
+    if(node == undefined || (<ts.ClassDeclaration>node).name == undefined){
       return;
     }
-
     let symbol = checker.getSymbolAtLocation((<ts.ClassDeclaration>node).name);
-    if (symbol == undefined) {
+    if(symbol == undefined){
       return;
     }
-    console.info(symbol.getName());
-    if (node.kind == ts.SyntaxKind.ModuleBlock) {
-      // if(symbol['parent'] != undefined){//} && symbol['parent']['SymbolObject'] != undefined && symbol['parent']['SymbolObject']['name'] != undefined){
-      // var filename:string = symbol['parent']['SymbolObject']['name'];
-      // filename = filename.replace(new RegExp("\"", 'g'), '');
-      // if(filename.indexOf('C:/Users/hermans.s.MAXXTONBV/projects/maxxton-frontend/services-library/src') == 0){
+    console.info(node.kind + ": " + symbol.getName());
+    if (node.kind === ts.SyntaxKind.ClassDeclaration || node.kind === ts.SyntaxKind.ModuleBlock) {
       // This is a top level class, get its symbol
-      var cache = [];
-
-      // fs.writeFileSync("output/" + symbol.getName() + ".json", JSON.stringify(symbol, function (key, value) {
-      //   if (typeof value === 'object' && value !== null) {
-      //     if (cache.indexOf(value) !== -1) {
-      //       // Circular reference found, discard key
-      //       return;
-      //     }
-      //     // Store value in our collection
-      //     cache.push(value);
-      //   }
-      //   return value;
-      // }, 2));
-
-      if(symbol.declarations != undefined && symbol.declarations[0] != undefined && symbol.declarations[0].parent != undefined && symbol.declarations[0].parent.getSourceFile() != undefined) {
-        if (symbol.declarations[0].parent.getSourceFile().fileName.indexOf('C:/Users/hermans.s.MAXXTONBV/projects/maxxton-frontend/services-library/src') == 0) {
-          output.push(serializeClass(symbol));
-        }
-      }
-
+      output.push(serializeClass(symbol));
       // No need to walk any further, class expressions/inner declarations
       // cannot be exported
-      // }
-      // }
-
     }
     else if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
       // This is a namespace, visit its children
@@ -100,7 +71,6 @@ function generateDocumentation(fileNames: string[], options: ts.CompilerOptions)
 
     // Get the construct signatures
     let constructorType = checker.getTypeOfSymbolAtLocation(symbol, symbol.valueDeclaration);
-    constructorType.get
     details.constructors = constructorType.getConstructSignatures().map(serializeSignature);
     return details;
   }
@@ -113,13 +83,11 @@ function generateDocumentation(fileNames: string[], options: ts.CompilerOptions)
       documentation: ts.displayPartsToString(signature.getDocumentationComment())
     };
   }
-
-  /** True if this is visible outside this file, false otherwise */
-  function isNodeExported(node: ts.Node): boolean {
-    return (node.flags & ts.NodeFlags.Export) !== 0 || (node.parent && node.parent.kind === ts.SyntaxKind.SourceFile);
-  }
 }
 
-generateDocumentation(process.argv.slice(2), {
+
+var sourcefile = ["test.ts"];
+
+generateDocumentation(sourcefile, {
   target: ts.ScriptTarget.ES5, module: ts.ModuleKind.CommonJS
 });
