b4aa654ee7b8ce570b75b469e543a8ecf1cb7aa0
Steven Hermans <steven166@live.nl>
2016-10-07 08:59:56 +0200

    Fix strange thinks

--- docs/demo/panels/endpoint-group-panel/endpoint-group.panel.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @author Steven Hermans
- */
-export declare class EndpointGroupPanel {
-    endpointGroup: any;
-    hidden: boolean;
-}
--- docs/demo/panels/endpoint-panel/endpoint.panel.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class EndpointPanel {
-    private endpoint;
-    private path;
-    getStatusName(statusCode: string): string;
-}
