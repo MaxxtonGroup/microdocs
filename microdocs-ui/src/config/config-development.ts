c73d43c81b0828abcaaaa2ea46672d14c8cba37a
s.hermans <s.hermans@maxxton.com>
2016-11-04 14:51:08 +0100

    Fix props

--- microdocs-ui/src/config/config-development.ts
+++ microdocs-ui/src/config/config-development.ts
@@ -7,7 +7,7 @@ export class MicroDocsConfig {
   public static isProduction:boolean = false;
   public static applicationName:string = "MicroDocs";
   public static profile:string = 'development';
-  public static basepath:string = "/";
+  public static basePath:string = "/";
   public static apiPath:string = "/api/v1";
   public static isStandAlone:boolean = false;
 }
--- microdocs-ui/src/config/config-standalone.ts
+++ microdocs-ui/src/config/config-standalone.ts
@@ -7,7 +7,7 @@ export class MicroDocsConfig {
   public static isProduction:boolean = true;
   public static applicationName:string = "MicroDocs";
   public static profile:string = 'production';
-  public static basepath:string = "/demo/";
+  public static basePath:string = "/demo/";
   public static apiPath:string = "/demo/data";
   public static isStandAlone:boolean = true;
 }
