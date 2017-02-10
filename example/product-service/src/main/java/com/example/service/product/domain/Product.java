9d45e51e75d2075f906e582cc74abc463b80620a
s.hermans <s.hermans@maxxton.com>
2017-02-10 12:47:34 +0100

    Fix product-server example

--- example/product-service/src/main/java/com/example/service/product/domain/Product.java
+++ example/product-service/src/main/java/com/example/service/product/domain/Product.java
@@ -2,8 +2,6 @@ package com.example.service.product.domain;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
@@ -11,11 +9,9 @@ import java.io.Serializable;
  *
  * @author Steven Hermans (s.hermans@maxxton.com)
  */
-@Entity
 public class Product implements Serializable{
 
     /** @example 15 */
-    @Id
     private Long productId;
 
     /** @example dam */
@@ -27,6 +23,14 @@ public class Product implements Serializable{
     @NotNull
     @NotEmpty
     private Double price;
+
+    public Product(){}
+
+    public Product(Long productId, String name, Double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
 
     public Long getProductId() {
         return productId;
